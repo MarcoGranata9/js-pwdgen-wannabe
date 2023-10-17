903c0f9f8fabddd7526e69c93ff0fefdeeb77e9f
MarcoGranata9 <granatam9900@gmail.com>
2023-10-17 16:25:37 +0200

    add: collected data chaining and message output

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 // Raccolta dati
 const name = prompt("Inserisci il tuo nome");
-console.log(name);
+console.log(name, typeof name);
 
 const surname = prompt("Inserisci il tuo cognome");
-console.log(surname)
+console.log(surname, typeof surname)
 
 const favcolor = prompt("Inserisci il tuo colore perferito");
-console.log(favcolor)
+console.log(favcolor, typeof favcolor)
+
+// Logica programma
+
+// Concatenazione
+// const password = name + surname + favcolor + "21";
+// console.log(password, typeof password)
+
+// Concatenazione template literal
+
+const password = `${name}${surname}${favcolor}21`
+console.log(password, typeof password)
+
+// Messaggio
+const message = `La tua password super sicura è ${password}!`
+console.log(message, typeof message)
+
+// Output
+document.getElementById("safepassword").innerHTML = message
+
 
 
 
